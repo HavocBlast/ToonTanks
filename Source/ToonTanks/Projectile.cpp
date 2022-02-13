7df4fd6c236aadc7db3aaa8e7da65dc8cbe6f495
Jacob Kruse <jrkruse@bananaclipstudio.com>
2022-02-12 19:43:05 -0500

    Completes Lecture 157

--- Source/ToonTanks/Projectile.cpp
+++ Source/ToonTanks/Projectile.cpp
@@ -24,7 +24,8 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectile::OnHit);
 }
 
 // Called every frame
@@ -32,5 +33,10 @@ void AProjectile::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+}
+
+void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("HitComp: %s, OtherActor: %s, OtherComp: %s"), *HitComp->GetName(), *OtherActor->GetName(), *OtherComp->GetName());
 }
 
